8dfd73f788d397380ed5d7922e4ffaf2d79e0b9f
Anastasiya Nitievskaya <anonim22@icloud.com>
2021-10-04 16:29:02 +0300

    feat: add adaptive

--- museum/js/index.js
+++ museum/js/index.js
@@ -10,17 +10,15 @@ progress2.addEventListener('input', function() {
     this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #fff ${value}%, white 100%)`
   })
 
-
- 
     const pictureInnerContainer = document.querySelector('.gallery_images');
-    const array = [ `assets/img/gallery/galery1.jpg`, `assets/img/gallery/galery2.jpg`, 
-                  `assets/img/gallery/galery3.jpg`, `assets/img/gallery/galery4.jpg`,
-                  `assets/img/gallery/galery5.jpg`, `assets/img/gallery/galery6.jpg`,
-                  `assets/img/gallery/galery7.jpg`, `assets/img/gallery/galery8.jpg`,
-                  `assets/img/gallery/galery9.jpg`, `assets/img/gallery/galery10.jpg`, 
-                  `assets/img/gallery/galery11.jpg`, `assets/img/gallery/galery12.jpg`,
-                  `assets/img/gallery/galery13.jpg`, `assets/img/gallery/galery14.jpg`,
-                  `assets/img/gallery/galery15.jpg` ]
+    const array = [ `assets/img/gallery/galery1.webp`, `assets/img/gallery/galery2.webp`, 
+                  `assets/img/gallery/galery3.webp`, `assets/img/gallery/galery4.webp`,
+                  `assets/img/gallery/galery5.webp`, `assets/img/gallery/galery6.webp`,
+                  `assets/img/gallery/galery7.webp`, `assets/img/gallery/galery8.webp`,
+                  `assets/img/gallery/galery9.webp`, `assets/img/gallery/galery10.webp`, 
+                  `assets/img/gallery/galery11.webp`, `assets/img/gallery/galery12.webp`,
+                  `assets/img/gallery/galery13.webp`, `assets/img/gallery/galery14.webp`,
+                  `assets/img/gallery/galery15.webp` ]
 
     function shuffle(array) {
       let sorted = array.sort(() => Math.random() - 0.5);
@@ -34,14 +32,13 @@ progress2.addEventListener('input', function() {
       img.alt = `galery1`;
       img.classList.add('gallery-img')
       pictureInnerContainer.append(img);
-      if(i == 0 || i == 11) {
+      if(i == 0 || i == 10) {
         img.style.paddingTop = '50' + "px";
         }
       
   });
 
-
-  const selectSingle = document.querySelector('.__select');
+const selectSingle = document.querySelector('.__select');
 const selectSingle_title = selectSingle.querySelector('.__select__title');
 const selectSingle_labels = selectSingle.querySelectorAll('.__select__label');
 
@@ -71,7 +68,7 @@ var btn = document.querySelector(".amount_tickets__buy");
 var span = document.querySelector('.close');
 // Когда пользователь нажимает кнопку, открывается pop-up форма 
 btn.onclick = function() {
-modal.style.display = 'block';
+modal.style.display = 'flex';
 }
 // Когда пользователь нажимает кнопку (x) <span>, закрывается окно формы
 span.onclick = function() {
@@ -84,37 +81,84 @@ window.onclick = function(event) {
   }
 }
 
+//menu
+const input = document.querySelector('#menu-toggle');
+const h2 = document.querySelector('.welcome_block h2');
+const p = document.querySelector('.welcome_block p');
+const a = document.querySelector('.welcome_block a');
+const menubox = document.querySelector('.menubox');
 
-console.log(`Score: 150 / 150
-Вёрстка валидная +10
-Вёрстка семантическая. В коде страницы присутствуют следующие элементы (указано минимальное количество, может быть больше) +24
-Вёрстка соответствует макету +45
-Форма покупки билетов +18
-В открытом состоянии под формой есть полупрозрачный overlay, который занимает весь экран. Форма и overlay прокручиваются вместе со страницей +1
-форма открывается при клике по кнопке Buy Now в секции Tickets и закрывается кликом по иконке с крестиком в верхнем правом углу или кликом по overlay +2
-при вёрстке формы используются следующие элементы: form, input type="text", input type="email", input type="tel", input type="number" +5
-вёрстка формы соответствует макету + 10
-Требования к css + 17
-добавлен favicon +2
-для построения сетки используются флексы или гриды +2
-при уменьшении масштаба страницы браузера вёрстка размещается по центру, а не сдвигается в сторону +2
-фоновый цвет каждого блока и секции тянется на всю ширину страницы +2
-иконки добавлены в формате .svg. +2
-расстояние между буквами, там, где это требуется по макету, регулируется css-свойством letter-spacing +2
-переключаются радиокнопки в блоке Tickets, одновременно может быть выбрана только одна кнопка +2
-в блоке Contacts правильно указаны ссылки на почту mailto и на телефон tel +2
-в футере добавлены ссылки на соцсети. +1
-Интерактивность, реализуемая через css +25
-плавная прокрутка по якорям +5
-параллакс +5
-при кликам по кнопке Discover the Louvre и карточкам секции Visiting открываются полноэкранные панорамы Google Street View встроенные в страницы вашего сайта при помощи iframe +5
-изменение стиля интерактивных элементов при наведении и клике +10
-интерактивность включает в себя не только изменение внешнего вида курсора, например, при помощи свойства cursor: pointer, но и другие визуальные эффекты – изменение цвета фона или шрифта, появление подчёркивания и т.д. Если в макете указаны стили при наведении и клике, для элемента указываем эти стили. Если в макете стили не указаны, реализуете их по своему усмотрению, руководствуясь общим стилем макета +4
-обязательное требование к интерактивности: плавное изменение внешнего вида элемента при наведении и клике не влияющее на соседние элементы +2
-интерактивность при наведении карточек в секции Visiting предусматривает плавное растягивание подчёркивания заголовка карточки на всю ширину карточки +2
-интерактивность при наведении иконок социальных сетей в футере предусматривает изменение цвета иконки и круглой границы вокруг иконки на золотистый +2
-Интерактивность, реализуемая через js +14
-можно передвигать ползунки громкости и прогресс-бар видео, при этом цвет шкалы до и после ползунка отличается и соответствует макету +2
-кликами по кнопкам + и - в секции Tiskets можно менять количество билетов Basic и Senior от 0 до 20 +2
-при перезагрузке (обновлении) страницы картины в блоке Galery отображаются в рандомном порядке + 10
-Итого: 153 балла.`)
+function check(input) {
+  if (input.checked) {
+    a.style.marginLeft = '-100px';
+    h2.style.marginLeft = '-150px';
+    p.style.marginLeft = '-150px';
+  } else {
+    a.style.marginLeft = '0px';
+    h2.style.marginLeft = '0px';
+    p.style.marginLeft = '0px';
+  }
+}
+menubox.addEventListener('click', function() {
+  if (input.checked) {
+    input.checked = false;
+    a.style.marginLeft = '0px';
+    h2.style.marginLeft = '0px';
+    p.style.marginLeft = '0px';
+  }
+})
+
+
+
+function findVideos() {
+  let videos = document.querySelectorAll('.video');
+
+  for (let i = 0; i < videos.length; i++) {
+      setupVideo(videos[i]);
+  }
+}
+
+function setupVideo(video) {
+  let link = video.querySelector('.video__link');
+  let media = video.querySelector('.video__media');
+  let button = video.querySelector('.btn_youtube');
+  let id = parseMediaURL(media);
+
+  video.addEventListener('click', () => {
+      let iframe = createIframe(id);
+
+      link.remove();
+      button.remove();
+      video.appendChild(iframe);
+  });
+
+  link.removeAttribute('href');
+  video.classList.add('video--enabled');
+}
+
+function parseMediaURL(media) {
+  let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/mqdefault\.jpg/i;
+  let url = media.src;
+  let match = url.match(regexp);
+
+  return match[1];
+}
+
+function createIframe(id) {
+  let iframe = document.createElement('iframe');
+
+  iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('allow', 'autoplay');
+  iframe.setAttribute('src', generateURL(id));
+  iframe.classList.add('video__media');
+
+  return iframe;
+}
+
+function generateURL(id) {
+  let query = '?rel=0&showinfo=0&autoplay=1';
+
+  return 'https://www.youtube.com/embed/' + id + query;
+}
+
+findVideos();
