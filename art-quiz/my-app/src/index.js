45296b5aab638f7ed2990c772f6189bb3621fb1c
Anastasiya Nitievskaya <anonim22@icloud.com>
2021-11-22 18:02:35 +0300

    fixed: delete folder

--- art-quiz/my-app/src/index.js
+++
@@ -1,6 +0,0 @@
-import "./modules/main-page";
-import "./modules/settings-page";
-import "./modules/category-painters";
-import "./modules/category-images";
-import "./modules/pictures-result";
-import "./modules/pictures-result2";
